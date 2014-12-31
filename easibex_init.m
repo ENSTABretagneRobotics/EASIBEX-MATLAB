0574f4af2836f91dfb5de81eb193fb0226825a50
lebarsfa <fabrice.le_bars@ensta-bretagne.fr>
2014-12-31 19:37:16 +0100

    Added comments

--- easibex_init.m
+++ easibex_init.m
@@ -1,3 +1,8 @@
+% Ideally, you should call : 
+% easibex_init
+% in the beginning (otherwise the initialization will be done when necessary but might take several seconds), and :
+% unloadlibrary('intervalx_adapt');
+% at the end (this will not be done automatically, but it should not be important in most cases...).
 switch (computer)
     case 'PCWIN64'
         loadlibrary('intervalx_adapt', @intervalx_adapt_proto);
--- easibex_setup_x64.m
+++ easibex_setup_x64.m
@@ -1,3 +1,13 @@
+% Run this file to make MATLAB 64 bit generate important precompiled files : 
+% intervalx_adapt_proto.m and intervalx_adapt_thunk_pcwin64.dll.
+% Visual Studio 2008 with 64 bit compiler needs to be installed and
+% correctly configured in MATLAB (using mex -setup and mex -setup C++ in
+% MATLAB).
+% These files will be necessary for MATLAB 64 bit users that do not have
+% Visual Studio. 
+% Note that this problem does not exist for MATLAB 32 bit (for several 
+% reasons, such as the fact that it is provided with a 32 bit internal 
+% compiler and therefore do not need Visual Studio...).
 switch (computer)
     case 'PCWIN64'
         loadlibrary('intervalx_adapt', 'intervalx_adapt.h', 'notempdir')
